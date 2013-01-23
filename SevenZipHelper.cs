582e2b5ea03730468384b50cd61cfd3f60e1ba87
unknown <tomasz@ciborski.comgit config --global user.email tomasz@ciborski.com>
2013-01-23 21:29:17 +0000

    Better code

--- SevenZipHelper.cs
+++ SevenZipHelper.cs
@@ -1,23 +1,33 @@
 using System;
 using System.IO;
+using System.Text;
 
 namespace SevenZip
 {
-	public static class Helper
-	{
-		#region Settings
-		
-		static Int32 dictionary = 1 << 23; // 8 MB
+    public static class Helper
+    {
+        #region Settings
 
-		static Int32 posStateBits = 2;
-		static Int32 litContextBits = 3;
-		static Int32 litPosBits = 0;
-		static Int32 algorithm = 2;
-		static Int32 numFastBytes = 128;
+        /// <summary>
+        /// 2 MB of memory will be reserved for dictionary.
+        /// </summary>
+        static Int32 dictionary = 1 << 21; // 2 MB
 
-		static bool eos = false;
-		
-		static CoderPropID[] propIDs =
+        static Int32 posStateBits = 2;
+        static Int32 litContextBits = 3;
+        static Int32 litPosBits = 0;
+        static Int32 algorithm = 2;
+
+        /// <summary>
+        /// Incerease numFastBytes to get better compression ratios.
+        /// 32 - moderate compression
+        /// 128 - extreme compression
+        /// </summary>
+        static Int32 numFastBytes = 32;
+
+        static bool eos = false;
+
+        static CoderPropID[] propIDs =
 		{
 			CoderPropID.DictionarySize,
 			CoderPropID.PosStateBits,
@@ -28,8 +38,8 @@ namespace SevenZip
 			CoderPropID.MatchFinder,
 			CoderPropID.EndMarker
 		};
-		
-		static object[] properties =
+
+        static object[] properties =
 		{
 			(Int32)(dictionary),
 			(Int32)(posStateBits),
@@ -40,55 +50,67 @@ namespace SevenZip
 			"bt4",
 			eos
 		};
-		
-		#endregion
-		
-		/// <summary>
-		/// Compress stream using LZMA.
-		/// Remeber to set desirable positions in input and output streams.
-		/// </summary>
-		/// <param name="inStream">Input stream</param>
-		/// <param name="outStream">Output stream</param>
-		public static void Compress(Stream inStream, ref Stream outStream)
-		{
-			Compression.LZMA.Encoder encoder = new Compression.LZMA.Encoder();
-			
-			encoder.SetCoderProperties(propIDs, properties);
-			encoder.WriteCoderProperties(outStream);
-			
-			Int64 fileSize = inStream.Length;
-			for (int i = 0; i < 8; i++)
-				outStream.WriteByte((Byte)(fileSize >> (8 * i)));
-			
-			encoder.Code(inStream, outStream, -1, -1, null);
-		}
 
-		/// <summary>
-		/// Decompress LZMA compressed stream into outStream.
-		/// Remeber to set desirable positions in input and output streams.
-		/// </summary>
-		/// <param name="inStream">Input stream</param>
-		/// <param name="outStream">Output stream</param>
-		public static void Decompress(Stream inStream, ref Stream outStream)
-		{
-			byte[] properties = new byte[5];
-			if (inStream.Read(properties, 0, 5) != 5)
-				throw (new Exception("Input stream is too short."));
-			
-			Compression.LZMA.Decoder decoder = new Compression.LZMA.Decoder();
-			decoder.SetDecoderProperties(properties);
-			
-			long outSize = 0;
-			for (int i = 0; i <8; i++)
-			{
-				int v = inStream.ReadByte();
-				if (v < 0)
-					throw (new Exception("Input stream is too short."));
-				outSize |= ((long)(byte)v) << (8 * i);
-			}
-			
-			long compressedSize = inStream.Length - inStream.Position;
-			decoder.Code(inStream, outStream, compressedSize, outSize, null);
-		}
-	}
+        #endregion
+
+        /// <summary>
+        /// Compress input stream into output stream using LZMA.
+        /// Remeber to set desirable positions input and output streams.
+        /// Input reading starts at inStream.Position and ends at inStream.Length.
+        /// Output writing starts at outStream.Position.
+        /// </summary>
+        /// <param name="inStream">Input stream</param>
+        /// <param name="outStream">Output stream</param>
+        public static void Compress(Stream inStream, Stream outStream)
+        {
+            Compression.LZMA.Encoder encoder = new Compression.LZMA.Encoder();
+
+            encoder.SetCoderProperties(propIDs, properties);
+            encoder.WriteCoderProperties(outStream);
+
+            var writer = new BinaryWriter(outStream, Encoding.UTF8);
+            // Write original size.
+            writer.Write(inStream.Length - inStream.Position);
+
+            // Save position with compressed size.
+            long positionForCompressedSize = outStream.Position;
+            // Leave placeholder for size after compression.
+            writer.Write((Int64)0);
+
+            long positionForCompressedDataStart = outStream.Position;
+            encoder.Code(inStream, outStream, -1, -1, null);
+            long positionAfterCompression = outStream.Position;
+
+            // Seek back to the placeholder for compressed size.
+            outStream.Position = positionForCompressedSize;
+
+            // Write size after compression.
+            writer.Write((Int64)(positionAfterCompression - positionForCompressedDataStart));
+
+            // Restore position.
+            outStream.Position = positionAfterCompression;
+        }
+
+
+        /// <summary>
+        /// Decompress LZMA compressed stream into outStream.
+        /// Remeber to set desirable positions in input and output streams.
+        /// </summary>
+        /// <param name="inStream">Input stream</param>
+        /// <param name="outStream">Output stream</param>
+        public static void Decompress(Stream inStream, Stream outStream)
+        {
+            byte[] properties = new byte[5];
+            if (inStream.Read(properties, 0, 5) != 5)
+                throw (new Exception("Input stream is too short."));
+
+            Compression.LZMA.Decoder decoder = new Compression.LZMA.Decoder();
+            decoder.SetDecoderProperties(properties);
+
+            var br = new BinaryReader(inStream, Encoding.UTF8);
+            long decompressedSize = br.ReadInt64();
+            long compressedSize = br.ReadInt64();
+            decoder.Code(inStream, outStream, compressedSize, decompressedSize, null);
+        }
+    }
 }
